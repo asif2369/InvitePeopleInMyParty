f9c0ad6e167838bab5038265739e52aa56bde89f
Asif Reza <hridoy2369@gmail.com>
2023-07-13 20:17:04 +0600

    CSS done. Ready for Vue code

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <h1>People invited to my Party</h1>
+            <div class="input-container">
+                <input type="text" placeholder="Name..." />
+
+                <select>
+                    <option>Male</option>
+                    <option>Female</option>
+                </select>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: flex;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+    background-color: #e3f2ff;
+}
+
+h1 {
+    font-size: 3rem;
+    font-weight: bold;
+}
+
+.container {
+    padding: 3rem;
+}
+
+input,
+select {
+    width: 100%;
+    padding: 1rem;
+    margin-bottom: 0.5rem;
+    font-size: 1.5rem;
+    border: 1px solid #222;
+}
+</style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>
+        <!-- #226E6F' : '#6A1102' -->
+    </div>
+</template>
+
+<style scoped>
+div {
+    background-color: lightblue;
+    border-radius: 5px;
+    margin-top: 0.5rem;
+    text-align: center;
+    padding: 0.5rem;
+    color: #fff;
+    font-size: 1.5rem;
+}
+</style>
